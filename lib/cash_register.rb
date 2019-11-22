9ca50d54137c9df89986647456d5d818938dd88b
ParkerBacall <pbacall2@gmail.com>
2019-11-22 14:04:37 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
 
-    def initialize total, employee_discount = 0
-        @total = total
+    def initialize employee_discount = 0
+        @total = 0
         @employee_discount = employee_discount
     end 
 end
